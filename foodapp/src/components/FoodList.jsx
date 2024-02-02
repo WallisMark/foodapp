7cfd9d8430da5a1632e5d1b0b480b3463222000b
wallis <wallismark@gmail.com>
2024-02-02 05:25:06 +0200

    add components that loops over the data from the api

---
+++ foodapp/src/components/FoodList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import FoodIterm from '../FoodIterm'
+
+export default function FoodList({foodData}) {
+  return (
+    <div>
+       {
+      foodData.map((food)=>
+      <FoodIterm key={food.id} food={food}/>
+      )}
+    </div>
+  )
+}
