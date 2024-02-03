21222fa40c19dba8f7bba66e7029359052786aa0
wallis <wallismark@gmail.com>
2024-02-03 15:32:22 +0200

    fetch add display the food details

---
+++ foodapp/src/components/FoodDetails.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react'
+
+export default function FoodDetails({foodId}) {
+    const[food,setFood]= useState({})
+    const[isLoading,setIsLoading] = useState(true)
+    const URL = `https://api.spoonacular.com/recipes/${foodId}/information`
+    const API_KEY = "c829d60b13ac4536b30208b064a0ff2f"
+    useEffect((
+        ()=>{
+            async function fetchDetails(){
+                const res = await fetch(`${URL}?apiKey=${API_KEY}`);
+                const data = await res.json();
+                console.log(data)
+                setFood(data)
+                setIsLoading(false)
+            }
+            fetchDetails()
+        }
+    ),[foodId])
+  return (
+    <div>
+      <div>
+           <h1>{food.title}</h1>
+           <img src={food.image} alt="image"/>
+      
+         <div>
+           <span><strong>{food.readyInMinutes} Minutes</strong></span>
+           <span><strong>Serves {food.servings} people</strong></span>
+           <span>{food.vegetarian ? "vegetarian" :"Non-vegetarian"}</span>
+           <span>{food.vegan ? "Vegan" :""}</span>
+         </div>
+         <div>
+            <span> ${parseFloat((food.pricePerServing/100).toPrecision(3))} Per Serving</span>
+         </div>
+        
+         <h2>Dish Types</h2>
+         <div>
+            <ol>
+                {isLoading? <p>Data is isLoading...</p>:food.dishTypes.map((dishType)=><li>{dishType}</li>)} 
+            </ol>
+         </div>
+
+         <h2>Instructions</h2>
+         <div>
+            <ol>
+              {isLoading? <p>Data is isLoading...</p>: food.analyzedInstructions[0].steps.map((step)=><li>{step.step}</li>)}
+            </ol>
+            
+         </div>
+        </div>
+      
+    </div>
+  )
+}
